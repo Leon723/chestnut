74d57dc85ef6b11503e222aa909af6e8e8fc1645
LeonZhang <zhangliyang@zhangliyang.name>
2016-04-09 16:01:47 +0800

    update to 0.7.9
    update to 0.7.9

--- src/Database/Schema/MysqlSchema.php
+++ src/Database/Schema/MysqlSchema.php
@@ -24,6 +24,10 @@ class MysqlSchema extends Schema {
 		return $this->addColumn($column, 'tinyint', $length, $nullable, $default);
 	}
 
+	public function double($column, $length = 255, $nullable = false, $default = '') {
+		return $this->addColumn($column, 'float', $length, $nullable, $default);
+	}
+
 	public function toSQL() {
 		$result = "CREATE TABLE " . $this->table
 			. " (";
